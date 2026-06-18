200c44314e38ed6f4ae9ba31d592ba9e763cee82
Nick F1 <Nick F1@host.docker.internal>
2026-06-19 01:26:10 +0300

    Removed example from Java 7 book

--- src/PascalTriangle.java
+++ src/PascalTriangle.java
@@ -26,26 +26,6 @@ public class PascalTriangle {
 	}
 	
 	public static void main(String[] args) {
-		// old part
-		int p[][] = new int[LINES][];
-		
-		p[0] = new int[1];
-		System.out.println(p[0][0] = 1);
-		
-		p[1] = new int[2];
-		p[1][0] = p[1][1] = 1;
-		System.out.println(p[1][0] + " " + p[1][1]);
-		
-		for(int i = 2; i < LINES; i++) {
-			p[i] = new int[i+1];
-			System.out.print((p[i][0] = 1) + " ");
-			for(int j = 1; j < i; j++) {
-				System.out.print((p[i][j] = p[i-1][j-1] + p[i-1][j]) + " ");
-			}
-			System.out.println(p[i][i] = 1);
-		}
-		//----------------------------------
-		
 		int digits = (LINES == 0) ? 1 : (int) (LINES * Math.log10(2) - 0.5 * Math.log10(Math.PI * LINES / 2.0)) + 1;
 		int number[][] = new int[LINES][];
 		int field = LINES%2 == 0 ? LINES - 1 : LINES;
